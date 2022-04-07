18667466a52ae066018419496cf83d25acecbeaa
sergey <oborok05@bk.ru>
2022-04-07 15:09:39 +0300

    error-lesson6

--- geekshop/basket/views.py
+++ geekshop/basket/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from mainapp.models import Product
 from basket.models import Basket
 from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -22,3 +24,18 @@ def basket_add(request, id):
 def basket_remove(request, basket_id):
     Basket.objects.get(id=basket_id).delete()
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+def basket_edit(request, basket_id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=basket_id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user = request.user)
+        context = {"baskets": baskets}
+
+        result = render_to_string("basket/basket.html", context)
+        return JsonResponse({"result": result})
